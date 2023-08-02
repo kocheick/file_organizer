c8fa6dfeadcdcd7e5f44b5a3319f49b211a65a98
kocheicks <theshevapro@gmail.com>
2023-08-02 13:33:30 -0400

    loading screen contains app logo

--- app/src/main/java/com/shevapro/filesorter/ui/components/LoadingScreen.kt
+++ app/src/main/java/com/shevapro/filesorter/ui/components/LoadingScreen.kt
@@ -1,11 +1,25 @@
 package com.shevapro.filesorter.ui.components
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.runtime.Composable
-
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.shevapro.filesorter.R
+@Preview()
 @Composable
 fun LoadingScreen() {
 
-    CircularProgressIndicator()
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center){
+        CircularProgressIndicator(modifier = Modifier.size(84.dp))
+        Image(painter = painterResource(id = R.drawable.ic_launcher_foreground), contentDescription ="App logo." )
+    }
 
 }
