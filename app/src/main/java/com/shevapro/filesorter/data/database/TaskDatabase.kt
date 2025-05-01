2dae1853ca2c96041c0750d7f9253842ed64bc64
shevapro <theshevapro@gmail.com>
2025-05-01 19:16:13 -0400

    Add Rule entity and RuleConverters to TaskDatabase
    This commit introduces the Rule entity and its associated TypeConverters to TaskDatabase. It also updates the database version to 7 and adds a new ruleDao for handling Rule-related operations.

--- app/src/main/java/com/shevapro/filesorter/data/database/TaskDatabase.kt
+++ app/src/main/java/com/shevapro/filesorter/data/database/TaskDatabase.kt
@@ -4,16 +4,20 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.shevapro.filesorter.model.MoveStat
+import com.shevapro.filesorter.model.Rule
+import com.shevapro.filesorter.model.RuleConverters
 import com.shevapro.filesorter.model.TaskRecord
 
-@Database(entities = [TaskRecord::class,MoveStat::class], version = 5, exportSchema = false)
-//@TypeConverters(UriConverter::class)
+@Database(entities = [TaskRecord::class, MoveStat::class, Rule::class], version = 7, exportSchema = false)
+@TypeConverters(RuleConverters::class)
 
 abstract class TaskDatabase : RoomDatabase() {
 
     abstract fun taskDao(): TaskDao
     abstract fun appStats(): StatsDao
+    abstract fun ruleDao(): RuleDao
 
     companion object {
         private var INSTANCE: TaskDatabase? = null
